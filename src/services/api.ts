ed3e9c18a1e736bb7daea5cce30bc801a897ea31
Muhammad Ahmad Khan <muhammadahmaddd8@gmail.com>
2025-07-19 23:48:02 +0500

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://3-d-point-cloud-labeling-platform-b.vercel.app/api';
 
 export interface ApiResponse<T = any> {
   success: boolean;
