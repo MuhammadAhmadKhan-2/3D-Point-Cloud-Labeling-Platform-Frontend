7f36e689ef2f3e251ec2dc6539b97be6e4e4c97a
Muhammad Ahmad Khan <muhammadahmaddd8@gmail.com>
2025-07-19 23:47:38 +0500

    Update adminApi.ts

--- src/services/adminApi.ts
+++ src/services/adminApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://3-d-point-cloud-labeling-platform-b.vercel.app/api';
 
 export interface AdminUser {
   id: string;
